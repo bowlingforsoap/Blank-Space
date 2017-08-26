8e930f0da1c4c58ce3acce5712d8c9959d6ae581
Strelchenko Vadym <bowlingforsoap@gmail.com>
2017-08-26 16:32:36 +0200

    Paint a circle instead of a dot

--- Assets/Scripts/RayCaster.cs
+++ Assets/Scripts/RayCaster.cs
@@ -14,7 +14,7 @@ public class RayCaster : MonoBehaviour {
         Camera currentCam = GetComponent<Camera>();
         Ray ray = currentCam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
-        Debug.DrawRay(ray.origin, ray.direction);
+        Debug.DrawRay(ray.origin, ray.direction * 10.0f);
         if (Physics.Raycast(ray, out hit))
             print("I'm looking at " + hit.transform.name + " UV: " + hit.textureCoord);
 
@@ -31,7 +31,41 @@ public class RayCaster : MonoBehaviour {
         pixelUV.x *= tex.width;
         pixelUV.y *= tex.height;
 
-        tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
+		Circle (tex, (int)pixelUV.x, (int)pixelUV.y, 10, Color.clear);
+
+		/*Color texColor = tex.GetPixel ((int)pixelUV.x, (int)pixelUV.y);
+		Debug.Log ("texColor before: " + texColor);
+		texColor.a = 0.0f;
+		Debug.Log ("texColor before changed: " + texColor);
+
+        tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, texColor);
+		texColor = tex.GetPixel ((int)pixelUV.x, (int)pixelUV.y);
+		Debug.Log ("texColor after: " + texColor);*/
+
         tex.Apply();
     }
+
+	public void Circle(Texture2D tex, int cx, int cy, int r, Color col)
+	{
+		int x, y, px, nx, py, ny, d;
+
+		for (x = 0; x <= r; x++)
+		{
+			d = (int)Mathf.Ceil(Mathf.Sqrt(r * r - x * x));
+			for (y = 0; y <= d; y++)
+			{
+				px = cx + x;
+				nx = cx - x;
+				py = cy + y;
+				ny = cy - y;
+
+				tex.SetPixel(px, py, col);
+				tex.SetPixel(nx, py, col);
+
+				tex.SetPixel(px, ny, col);
+				tex.SetPixel(nx, ny, col);
+
+			}
+		}    
+	}
 }
