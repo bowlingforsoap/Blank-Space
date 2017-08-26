a9923060cba689f545a34e86247437da759ba0ca
Xavier Bonaventura <xavibonaventura@gmail.com>
2017-08-26 15:37:44 +0200

    Initial commit

---
+++ Assets/Scripts/RayCaster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCaster : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera currentCam = GetComponent<Camera>();
+        Ray ray = currentCam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        Debug.DrawRay(ray.origin, ray.direction);
+        if (Physics.Raycast(ray, out hit))
+            print("I'm looking at " + hit.transform.name + " UV: " + hit.textureCoord);
+
+
+
+        Renderer rend = hit.transform.GetComponent<Renderer>();
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+
+        if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+            return;
+
+        Texture2D tex = rend.material.mainTexture as Texture2D;
+        Vector2 pixelUV = hit.textureCoord;
+        pixelUV.x *= tex.width;
+        pixelUV.y *= tex.height;
+
+        tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
+        tex.Apply();
+    }
+}
